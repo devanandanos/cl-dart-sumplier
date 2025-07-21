9b716e71cb219c112747635da0d70832adec0633
devanandanos <devanandanos387@gmail.com>
2025-07-21 15:30:51 +0530

    Update pro.dart

--- docs/pro.dart
+++ docs/pro.dart
@@ -1,1 +1,22 @@
+import 'dart:io';
 
+void main() {
+  print("Multiplication Table Generator!\n");
+
+  stdout.write("Enter a number to generate its multiplication table: ");
+  int number = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+
+  print("\nMultiplication Table for $number:");
+  print("----------------------------------");
+
+  int sum = 0;
+
+  for (int i = 1; i <= 10; i++) {
+    int product = number * i;
+    print("$number x $i = $product");
+    sum += product;
+  }
+
+  print("----------------------------------");
+  print("Sum of the first 10 multiples of $number is: $sum");
+}
